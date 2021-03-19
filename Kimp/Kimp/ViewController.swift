1c5abaca62947a9a05c53a8527365b4339636242
김종권 <jongkwon.kim@42dot.com>
2021-03-19 22:10:39 +0900

    project 생성

---
+++ Kimp/Kimp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kimp
+//
+//  Created by 김종권 on 2021/03/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
